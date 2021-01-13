e013eefb17e20e5bb3c88423a9fcb8489dccbf0a
ametsch <22308539+ametsch@users.noreply.github.com>
2021-01-13 13:43:18 -0500

    Add files via upload

--- BankAccountClientV3.java
+++ BankAccountClientV3.java
@@ -1,4 +1,4 @@
-package Ch8Classes;
+package Ch8Classes.bankacct;
 
 import java.util.Scanner;
 
@@ -7,9 +7,9 @@ public class BankAccountClientV3 {
         Scanner scanner = new Scanner(System.in);
         
         BankAccountV2 test = new BankAccountV2("Aaron Metsch", "password", (short) 1234,
-                new DOB((byte) 16, (byte) 12, (short) 2003),
-                new Address(40, "Byron Lane", "Larchmont", "NY", 10538),
-                "test@gmail.com", "123456789", "9148337958" );
+                new DOB((byte) 6, (byte) 9, (short) 2016),
+                new Address(6969, "Main Street", "Larchmont", "NY", 10538),
+                "test@gmail.com", "123456789", "1234567890" );
 
         System.out.println(test);
     }
